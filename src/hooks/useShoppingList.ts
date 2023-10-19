a4563fe83af098d4723d1d4c729115a6fd052602
KarolinaFilipiuk <kfilipiuk@student.agh.edu.pl>
2023-10-19 17:05:21 +0200

    [GREEN] Implementation of custom hook for list management

--- src/hooks/useShoppingList.ts
+++ src/hooks/useShoppingList.ts
@@ -1,14 +1,33 @@
 import { useState } from 'react'
 
 export function useShoppingList() {
-  const [column1, setColumn1] = useState<string[]>()
-  const [column2, setColumn2] = useState<string[]>()
+  const [column1, setColumn1] = useState<string[]>([
+    'tomato',
+    'kiwi',
+    'apple',
+    'banana',
+    'pomelo',
+  ])
+  const [column2, setColumn2] = useState<string[]>([])
 
   const moveItem = (
     item: string,
     sourceColumn: number,
     targetColumn: number
-  ) => {}
+  ) => {
+    const sourceList = sourceColumn === 1 ? column1 : column2
+    const targetList = targetColumn === 1 ? column1 : column2
+
+    const updatedSourceList = sourceList.filter((i) => i !== item)
+    const updatedTargetList = [...targetList, item]
+
+    sourceColumn === 1
+      ? setColumn1(updatedSourceList)
+      : setColumn2(updatedSourceList)
+    targetColumn === 1
+      ? setColumn1(updatedTargetList)
+      : setColumn2(updatedTargetList)
+  }
 
   return { column1, column2, moveItem }
 }
