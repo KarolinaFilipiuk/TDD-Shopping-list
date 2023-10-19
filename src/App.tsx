88a8682db5247bbc059a52692475f0d0e2a78509
KarolinaFilipiuk <kfilipiuk@student.agh.edu.pl>
2023-10-19 17:24:08 +0200

    [GREEN] Implementation of App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import ShoppingList from './components/shopping-list/ShoppingList'
+import PickedList from './components/picked-list/PickedList'
+import { useShoppingList } from './hooks/useShoppingList'
 import './App.css'
 
 const App = () => {
-  return <div></div>
+  const { column1, column2, moveItem } = useShoppingList()
+
+  return (
+    <div className='App'>
+      <h1>Shop list</h1>
+      <div className='columns'>
+        <ShoppingList
+          items={column1}
+          onItemPick={(item) => moveItem(item, 1, 2)}
+        />
+        <PickedList
+          items={column2}
+          onItemUnpick={(item) => moveItem(item, 2, 1)}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default App
